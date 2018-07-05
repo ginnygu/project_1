a742470902ef064c3212c9b22c7c756fadbe19c4
ginnygu <ginnygu@gmail.com>
2018-07-05 12:38:11 -0400

    functional random face and random hole with click event

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,29 @@
-const holes = document.querySelectorAll('.holes');
-let instructors= ["aaron.png", "jz.png", "jason.png", "john.png", "drake.png"];
+const holes = document.querySelectorAll('.holes .faceholder');
+let instructors = document.querySelectorAll('#faces>img');
 let playerScore = 0;
-let currentHole;
-function pickOne(){
-    let x = Math.floor(Math.random() * holes.length);
-    let random = holes[x];
-    debugger;
-    return (random === currentHole) ? pickOne() : random;
-
-    
+let currentHole = chooseRandom(holes);
+let currentFace = chooseRandom(instructors);
+debugger;
+// @author Jason Seminara <js@ga.co>
+function chooseRandom(collection) {
+  return collection[Math.floor(Math.random() * collection.length)];
 }
+
+function chooseRandomWithoutCollision(collection) {
+    let newChoice;
+    do {
+        newChoice = chooseRandom(holes);
+    } while (newChoice === currentHole);
+    return newChoice;
+}
+
+
 
 
 function move(){
     let time = randomTime(100, 1000);
-    currentHole = pickOne();
-    
+    // current one
+    currentHole = chooseRandomWithoutCollision(holes);
 }
 
 function clicking (event) {
@@ -24,13 +32,16 @@ function clicking (event) {
 
     }
 
-addEventListener('click', clicking);
+document.querySelector('#gameholes').addEventListener('click', (event)=> {
+  const goodHitFace = event.path.includes(currentFace);
+  const goodHitHole = event.path.includes(currentHole);     
+    console.log(goodHitHole, goodHitFace);
+  debugger
+  if(goodHitFace && goodHitHole) {
+      // score points;
+  } else {
+      // no point
+  }
 
-function randomFaces(instructors){
-    
-      let randomize = Math.floor(Math.random() * instructors.length);
-      let choose = instructors[randomize];
+})
 
-      return choose;
-
-}
